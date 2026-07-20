7c80b6fb157804ad5ae91c158a2cca20ff374ae7
lushengjing <167280453+lushengjing@users.noreply.github.com>
2026-07-20 18:03:31 +0800

    更新注释

--- 温湿度气压MS8607/appv6.2/app/USER/BSP/main.c
+++ 温湿度气压MS8607/appv6.2/app/USER/BSP/main.c
@@ -12,25 +12,25 @@
 
 int main(void)
  {
-	SCB->VTOR = FLASH_BASE | 0x5000;//璁剧疆涓柇鍋忕Щ閲?
-	__enable_irq(); // 鍚敤鍏ㄥ眬涓柇锛圔ootloader璺宠浆鏃跺叧闂簡锛?
+	SCB->VTOR = FLASH_BASE | 0x5000;
+	__enable_irq(); 
 
 	HSE_SetSysClock(RCC_PLLMul_4);  //璁剧疆绯荤粺鏃堕挓涓猴細8MHZ * 4 = 32MHZ
-	delay_init();//寤舵椂鍒濆鍖?
+	delay_init();
 	MS8607_IIC_Config();
-	uart_init(115200);//usart1涓插彛鍒濆鍖?
+	uart_init(115200);//usart1
 	
 	Flash_Storage_Init();
-	IWDG_Init(6,2344); //婧㈠嚭鏃堕棿15s
+	IWDG_Init(6,2344); 
 	delay_ms(500);
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//涓柇鍒嗙粍
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
 
 	Modbus_uart2_init(9600);//485鍒濆鍖栵紙涓存椂浣跨敤9600锛?
-	TIM3_Int_Init(1000-1,32-1);//modbus瀹氭椂
-	TIM4_Int_Init(1000-1,32-1);//MMsec 1ms鍩哄噯
+	TIM3_Int_Init(1000-1,32-1);//modbus 1ms
+	TIM4_Int_Init(1000-1,32-1);//MMsec 1ms
 	Usart2_SendString("app V6.2\r\n");
 	delay_ms(100);
-	Modbus_Init();//鍒濆鍖栦粠鏈哄湴鍧€
+	Modbus_Init();//初始化modbus
 	
 	while(1)
 	{
